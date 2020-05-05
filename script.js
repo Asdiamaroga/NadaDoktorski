88e24b3875fb493ac49b54e87333c34e12b6afd1
Leon Csergity <93csergityleon@gmail.com>
2020-05-05 21:37:14 +0200

    added time stamps and statistics and all

--- script.js
+++ script.js
@@ -8,106 +8,132 @@ const numberOfButtons = 24;
 const buttonsPerRow = 6;
 const patterns = ['REPEATING', 'CHANGING']
 const predefinedButtonGrids = [
+	// ANALOG
 	{
+		name: 'A1.1',
 		colors: ['ANALOG-C11', 'ANALOG-C12', 'ANALOG-C13'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'A2.1',
 		colors: ['ANALOG-C11', 'ANALOG-C12', 'ANALOG-C13'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'A1.2',
 		colors: ['ANALOG-C21', 'ANALOG-C22', 'ANALOG-C23'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'A2.2',
 		colors: ['ANALOG-C21', 'ANALOG-C22', 'ANALOG-C23'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'A1.3',
 		colors: ['ANALOG-C31', 'ANALOG-C32', 'ANALOG-C33'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'A2.3',
 		colors: ['ANALOG-C31', 'ANALOG-C32', 'ANALOG-C33'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'A1.4',
 		colors: ['ANALOG-C41', 'ANALOG-C42', 'ANALOG-C43'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'A2.4',
 		colors: ['ANALOG-C41', 'ANALOG-C42', 'ANALOG-C43'],
 		pattern: patterns[1]
 	},
 	// complementary -------------------------------------
 	{
+		name: 'S1.1',
 		colors: ['COMP-C11', 'COMP-C12', 'COMP-C13'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'S2.1',
 		colors: ['COMP-C11', 'COMP-C12', 'COMP-C13'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'S1.2',
 		colors: ['COMP-C21', 'COMP-C22', 'COMP-C23'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'S2.2',
 		colors: ['COMP-C21', 'COMP-C22', 'COMP-C23'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'S1.3',
 		colors: ['COMP-C31', 'COMP-C32', 'COMP-C33'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'S2.3',
 		colors: ['COMP-C31', 'COMP-C32', 'COMP-C33'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'S1.4',
 		colors: ['COMP-C41', 'COMP-C42', 'COMP-C43'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'S2.4',
 		colors: ['COMP-C41', 'COMP-C42', 'COMP-C43'],
 		pattern: patterns[1]
 	},
 	// TRIADIC
 	{
+		name: 'T1.1',
 		colors: ['TRIADIC-C11', 'TRIADIC-C12', 'TRIADIC-C13'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'T2.1',
 		colors: ['TRIADIC-C11', 'TRIADIC-C12', 'TRIADIC-C13'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'T1.2',
 		colors: ['TRIADIC-C21', 'TRIADIC-C22', 'TRIADIC-C23'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'T2.2',
 		colors: ['TRIADIC-C21', 'TRIADIC-C22', 'TRIADIC-C23'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'T1.3',
 		colors: ['TRIADIC-C31', 'TRIADIC-C32', 'TRIADIC-C33'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'T2.3',
 		colors: ['TRIADIC-C31', 'TRIADIC-C32', 'TRIADIC-C33'],
 		pattern: patterns[1]
 	},
 	{
+		name: 'T1.4',
 		colors: ['TRIADIC-C41', 'TRIADIC-C42', 'TRIADIC-C43'],
 		pattern: patterns[0]
 	},
 	{
+		name: 'T2.4',
 		colors: ['TRIADIC-C41', 'TRIADIC-C42', 'TRIADIC-C43'],
 		pattern: patterns[1]
 	},
 	// CONTROL
 	{
+		name: 'K',
 		colors: ['CONTROL-C11', 'CONTROL-C11', 'CONTROL-C11'],
 		pattern: patterns[1]
 	}
@@ -120,19 +146,6 @@ var buttonGrids;
 
 
 
-
-// rework
-function calcTimeDifference(start) {
-	endTime = new Date();
-	stepEndTime = endTime;
-	// time difference in milliseconds
-	var timeDiff = endTime - start;
-	// strip the ms
-	timeDiff /= 1000;
-	// round to two decimalschangeVisibilityOfButtonGridStep('none');
-	var seconds = timeDiff.toFixed(2);
-	return seconds; 
-};
 
 function changeVisibilityOfPauseStep(visibility) {
 	document.querySelector('.pause-step').style.display = visibility;
@@ -160,6 +173,8 @@ function goToNextGrid() {
 	changeVisibilityOfButtonGridStep('none');
 	currentButtonGridIndex++;
 	const currentGrid = buttonGrids[currentButtonGridIndex];
+	currentGrid.startTimeOnGrid = new Date();
+
 	if(withPause) {
 		changeVisibilityOfPauseStep('block');
 		createThePauseButtonGrid(currentGrid.pattern, currentGrid.colors);
@@ -203,7 +218,9 @@ function createButtonGrid(pattern, colors, numberToAdd) {
 			num = numberToAdd;
 			numberToAdd = undefined;
 		} else {
-			num = getRandomNumberNotAlreadyContinedInArray(collectionOfNumbers, 100, 999, evenNumberCounter > 0 ? evenNumberCounter-- : evenNumberCounter);
+			//TODO rework this its ugly AF
+			let evenNumber = evenNumberCounter > 0 ? evenNumberCounter-- : evenNumberCounter;
+			num = getRandomNumberNotAlreadyContinedInArray(collectionOfNumbers, 100, 999, numberOfEvenNumbers != -1 ? evenNumber : undefined);
 		}
 		let btn = createButton(num);
 		colletionOfButtons.push(btn);
@@ -264,28 +281,78 @@ function createButton(content) {
 		event.currentTarget.disabled = true;
 		const currentGrid = buttonGrids[currentButtonGridIndex];
 
-		if ( currentGrid.evenNumbersClicked === undefined ) {
-			currentGrid.evenNumbersClicked = 0;
-		}
+		initializeSectionVariables(currentGrid);		
 
 		const numberInButton = parseInt(event.currentTarget.innerHTML, 10);
 		if(numberInButton % 2 === 0) {
-			currentGrid.evenNumbersClicked++;
+
+			if ( currentGrid.firstCorrectButtonClikcedAt === undefined ) {
+				currentGrid.firstCorrectButtonClikcedAt = calcTimeDifference(currentGrid.startTimeOnGrid);
+			}
+
+			if (!withPause) {
+				currentGrid.evenNumbersClicked++;
+			} else {
+				console.log('mistake made with EVEN');
+				currentGrid.numberOfMistakesMade++;
+			}
+		
 			const foundTheNumber = (withPause && numberInButton == currentGrid.numberToFind);
 			const foundAllEvenNumbers = (currentGrid.evenNumbersClicked == numberOfEvenNumbers);
 			const lastStep = currentButtonGridIndex == buttonGrids.length - 1;
 			if ((foundTheNumber || foundAllEvenNumbers) && !lastStep){
+
+				calculateTimeSpentOnPageAndFixMistakesNumber(currentGrid);
+				
 				goToNextGrid();				
 			} else if ((foundTheNumber || foundAllEvenNumbers) && lastStep) {
 				changeVisibilityOfButtonGridStep('none');
 				changeVisibilityOfPauseStep('none');
+				
+				calculateTimeSpentOnPageAndFixMistakesNumber(currentGrid);
+
 				document.querySelector('.thank-you-step').style.display = 'block';
+				console.log('result?????????', buttonGrids)
 			}
+
+		} else {
+			console.log('mistake made with ODD');
+			currentGrid.numberOfMistakesMade++;
 		}
 		
 	}
 
 	return btn;
+}
+
+function calculateTimeSpentOnPageAndFixMistakesNumber(currentGrid) {
+	currentGrid.timeSpentOnPage = calcTimeDifference(currentGrid.startTimeOnGrid);
+				//If we are here it was correct so not increasing this
+				if (withPause) {
+					currentGrid.numberOfMistakesMade--;
+				}
+}
+
+
+function calcTimeDifference(start) {
+	endTime = new Date();
+	// time difference in milliseconds
+	var timeDiff = endTime - start;
+	// strip the ms
+	timeDiff /= 1000;
+	// round to two decimalschangeVisibilityOfButtonGridStep('none');
+	var seconds = timeDiff.toFixed(2);
+	return seconds; 
+};
+
+function initializeSectionVariables(currentGrid) {
+	if ( currentGrid.evenNumbersClicked === undefined ) {
+		currentGrid.evenNumbersClicked = 0;
+	}
+
+	if( currentGrid.numberOfMistakesMade === undefined ) {
+		currentGrid.numberOfMistakesMade = 0;
+	}
 }
 
 function getRandomNumberNotAlreadyContinedInArray(array, min, max, even) {
@@ -297,17 +364,21 @@ function getRandomNumberNotAlreadyContinedInArray(array, min, max, even) {
 		numberExistInArray = array.includes(num);
 	}
 
-	//TODO not good really, idk
-	if (numberOfEvenNumbers == -1) {
+	// //TODO not good really, idk
+	// if (numberOfEvenNumbers == -1) {
+	// 	return num;
+	// }
+
+	if ( even === undefined ) {
 		return num;
 	}
 
 	//TODO rework, not so nice of a solution really
 	if (even) {
-		console.log('EVEn ', num % 2 == 0 ? num : num + 1)
+		// console.log('EVEn ', num % 2 == 0 ? num : num + 1)
 		return num % 2 == 0 ? num : num + 1;
 	} else {
-		console.log('ODD ', num % 2 !== 0 ? num : num + 1)
+		// console.log('ODD ', num % 2 !== 0 ? num : num + 1)
 		return num % 2 !== 0 ? num : num + 1;
 	}
 }
